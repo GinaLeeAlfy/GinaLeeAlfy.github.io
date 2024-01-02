d6579e8aeef22d6402949d77a3bdee1e77b46a48
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-01-02 12:56:32 -0800

    removed query client and added style.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./Home";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    },
-  },
-});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </QueryClientProvider>
+      <header>
+        <Link to="/">GinaLeeAlfy</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 };
