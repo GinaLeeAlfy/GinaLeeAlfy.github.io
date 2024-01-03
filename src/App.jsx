632512d52821b6c84c9e2500b035047914583552
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-01-02 16:52:29 -0800

    added icons from tabler and a light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import { useState } from "react";
 
 const App = () => {
+  const body = document.querySelector("body");
+  const [isLightMode, setLightMode] = useState("false");
+  const ToggleClass = () => {
+    setLightMode(!isLightMode);
+    body.classList.toggle("light");
+  };
+
   return (
     <BrowserRouter>
       <header>
-        <Link to="/">GinaLeeAlfy</Link>
+        <h1>
+          <Link to="/">GinaLeeAlfy</Link>
+        </h1>
+        <button className="switch" onClick={ToggleClass}>
+          <div className={isLightMode ? "circle" : "circle hidden"}></div>
+          <div className={isLightMode ? "circle hidden" : "circle"}></div>
+        </button>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,38 @@
+import { IconBrandGithub, IconBrandX } from "@tabler/icons-react";
+
 const Home = () => {
   return (
-    <div>
-      <p>hello</p>
+    <div className="container">
+      <section>
+        <div className="img-container">
+          <img
+            src="https://avatars.githubusercontent.com/u/19276818?v=4"
+            alt="Picture of Gina in the snow."
+          />
+        </div>
+      </section>
+      <section>
+        <div className="links">
+          <a href="https://github.com/GinaLeeAlfy" target="_blank">
+            <IconBrandGithub />
+            Github
+          </a>
+          <a href="https://twitter.com/GinaLeeAlfy" target="_blank">
+            <IconBrandX /> X/Twitter
+          </a>
+          <a href="https://icodethis.com/GinaLeeAlfy" target="_blank">
+            iCodeThis
+          </a>
+        </div>
+      </section>
+      <section>
+        <h2>Gina Alfandary Magana</h2>
+        <p>
+          I'm an aspiring web developer. I studied Human Biology at UCSC. In my
+          free time I enjoy video/board games, horseback riding, racket sports,
+          and anime.
+        </p>
+      </section>
     </div>
   );
 };
