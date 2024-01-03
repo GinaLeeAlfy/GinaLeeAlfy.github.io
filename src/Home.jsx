9f4ebd16e784ca5ba30936ce200795fd9e2c20e9
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-01-03 09:05:41 -0800

    fixed eslint suggestions

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,20 +7,32 @@ const Home = () => {
         <div className="img-container">
           <img
             src="https://avatars.githubusercontent.com/u/19276818?v=4"
-            alt="Picture of Gina in the snow."
+            alt="Gina in the snow."
           />
         </div>
       </section>
       <section>
         <div className="links">
-          <a href="https://github.com/GinaLeeAlfy" target="_blank">
+          <a
+            href="https://github.com/GinaLeeAlfy"
+            target="_blank"
+            rel="noreferrer"
+          >
             <IconBrandGithub />
             Github
           </a>
-          <a href="https://twitter.com/GinaLeeAlfy" target="_blank">
+          <a
+            href="https://twitter.com/GinaLeeAlfy"
+            target="_blank"
+            rel="noreferrer"
+          >
             <IconBrandX /> Twitter/X
           </a>
-          <a href="https://icodethis.com/GinaLeeAlfy" target="_blank">
+          <a
+            href="https://icodethis.com/GinaLeeAlfy"
+            target="_blank"
+            rel="noreferrer"
+          >
             iCodeThis
           </a>
         </div>
@@ -28,7 +40,7 @@ const Home = () => {
       <section>
         <h2>Gina Alfandary Magana</h2>
         <p>
-          I'm an aspiring web developer. I studied Human Biology at UCSC. In my
+          I am an aspiring web developer. I studied Human Biology at UCSC. In my
           free time I enjoy video/board games, horseback riding, racket sports,
           and anime.
         </p>
